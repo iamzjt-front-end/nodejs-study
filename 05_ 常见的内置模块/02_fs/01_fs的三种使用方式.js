6a6fab312fa620011051bfcd37e35008635d50de
iamzjt-front-end <iamzjt@foxmail.com>
2022-02-19 13:50:17 +0800

    fs

--- 05_ 常见的内置模块/02_fs/01_fs的三种使用方式.js
+++ 05_ 常见的内置模块/02_fs/01_fs的三种使用方式.js
@@ -4,9 +4,29 @@ const fs = require("fs");
 const filepath = "./abc.txt";
 
 // 1. 方式一：同步操作
-const info = fs.statSync(filepath);
-console.log("后续需要执行的代码");
-console.log(info);
+// const info = fs.statSync(filepath);
+// console.log("后续需要执行的代码");
+// console.log(info);
+
+// 2. 方式二：异步操作
+// fs.stat(filepath, (err, info) => {
+//   if (err) {
+//     console.log(err);
+//     return;
+//   } else {
+//     console.log(info);
+//   }
+// });
+
+// 3. 方式三：promise方式
+fs.promises
+  .stat(filepath)
+  .then((info) => {
+    console.log(info);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 // 文件信息
 // Stats {
