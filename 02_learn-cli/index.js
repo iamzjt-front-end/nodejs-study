608c883cffb1a0367ce2b0a0c48c28d5682a2f9d
iamzjt-front-end <iamzjt@foxmail.com>
2022-03-07 11:45:42 +0800

    program.opts().dest

--- 02_learn-cli/index.js
+++ 02_learn-cli/index.js
@@ -7,6 +7,16 @@ program.version(require("./package.json").version);
 
 // 增加自己的options
 program.option("-z --zjt", "a zjt cli");
-program.option("-d --dest", "a destination folder, 例如：-d /src/components");
+program.option(
+  "-d --dest <dest>",
+  "a destination folder, 例如：-d /src/components"
+);
+
+program.on("--help", function () {
+  console.log("Others:");
+  console.log("  other options~");
+});
 
 program.parse(process.argv);
+
+console.log(program.opts().dest);
