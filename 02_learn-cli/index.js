049632dc9d9f0016b28ebbd5382c6d1c93e3207e
iamzjt-front-end <iamzjt@foxmail.com>
2022-03-07 11:16:49 +0800

    ..

--- 02_learn-cli/index.js
+++ 02_learn-cli/index.js
@@ -2,6 +2,11 @@
 
 const program = require("commander");
 
-program.version("1.0.0");
+// 查看版本号
+program.version(require("./package.json").version);
+
+// 增加自己的options
+program.option("-z --zjt", "a zjt cli");
+program.option("-d --dest", "a destination folder, 例如：-d /src/components");
 
 program.parse(process.argv);
