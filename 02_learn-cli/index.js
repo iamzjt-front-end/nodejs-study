a5739198509b1b414c468b9d41fc88e99ad1c6c2
iamzjt-front-end <iamzjt@foxmail.com>
2022-03-07 19:28:39 +0800

    create

--- 02_learn-cli/index.js
+++ 02_learn-cli/index.js
@@ -3,6 +3,7 @@
 const program = require("commander");
 
 const helpOptions = require("./lib/core/help");
+const createCommands = require("./lib/core/create");
 
 // 查看版本号
 program.version(require("./package.json").version);
@@ -10,6 +11,9 @@ program.version(require("./package.json").version);
 // 帮助和可选信息
 helpOptions();
 
+// 创建其他指令
+createCommands();
+
 program.parse(process.argv);
 
-console.log(program.opts().dest);
+// console.log(program.opts().dest);
---
+++ 02_learn-cli/lib/core/create.js
@@ -0,0 +1,12 @@
+const program = require("commander");
+
+const createCommands = () => {
+  program
+    .command("create <project> [others...]")
+    .description("clone repository into a folder")
+    .action((project, others) => {
+      console.log(project, others);
+    });
+};
+
+module.exports = createCommands;
