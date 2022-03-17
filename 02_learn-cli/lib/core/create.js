e6d766de8cc934ee44734fcbb941c3e47a32f247
iamzjt-front-end <iamzjt@foxmail.com>
2022-03-18 00:00:40 +0800

    。。

--- 02_learn-cli/lib/core/create.js
+++ 02_learn-cli/lib/core/create.js
@@ -1,12 +1,14 @@
 const program = require("commander");
 
+const {
+  createProjectAction
+} = require("./actions");
+
 const createCommands = () => {
   program
     .command("create <project> [others...]")
     .description("clone repository into a folder")
-    .action((project, others) => {
-      console.log(project, others);
-    });
+    .action(createProjectAction);
 };
 
 module.exports = createCommands;
