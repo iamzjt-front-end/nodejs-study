7a532dd3cc17d4e6b86fc03f221de9f58492676d
iamzjt-front-end <iamzjt@foxmail.com>
2022-03-29 08:28:42 +0800

    下载模版代码

--- 02_learn-cli/lib/core/actions.js
+++ 02_learn-cli/lib/core/actions.js
@@ -1,12 +1,16 @@
-const createProjectAction = (project) => {
+const { promisify } = require("util");
+
+const download = promisify(require("download-git-repo"));
+
+const createProjectAction = async (project) => {
   // 1. clone项目
-  
+  await download();
   // 2. 执行 npm install
 
   // 3. 运行 npm run serve
 
   // 4. 打开浏览器
-
+  
 };
 
 module.exports = {
