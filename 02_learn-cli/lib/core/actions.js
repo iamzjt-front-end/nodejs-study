8ff7f598481aab7188dc1e2ffed58074b3329cb1
iamzjt-front-end <iamzjt@foxmail.com>
2022-04-03 19:29:09 +0800

    增加提示

--- 02_learn-cli/lib/core/actions.js
+++ 02_learn-cli/lib/core/actions.js
@@ -13,11 +13,12 @@ const createProjectAction = async (project) => {
   await download(vueRepo, project, { clone: true });
 
   // 2. 执行 npm install
-  console.log("正在安装依赖，请稍候...");
+  console.log("项目克隆完成，正在安装依赖，请稍候...");
   const command = process.platform === "win32" ? "npm.cmd" : "npm";
   await commandSpawn(command, ["install"], { cwd: `./${project}` });
 
   // 3. 运行 npm run serve
+  console.log("依赖安装完成，正在启动项目，请稍候...");
   await commandSpawn(command, ["run", "serve"], { cwd: `./${project}` });
 
   // 4. 打开浏览器
