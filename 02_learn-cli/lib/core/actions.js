85da43980039acbba5f075b19937f078266b1979
iamzjt-front-end <iamzjt@foxmail.com>
2022-04-03 19:09:48 +0800

    执行 npm install

--- 02_learn-cli/lib/core/actions.js
+++ 02_learn-cli/lib/core/actions.js
@@ -1,14 +1,16 @@
 const { promisify } = require("util");
 
 const download = promisify(require("download-git-repo"));
-
 const { vueRepo } = require("../config/repo-config");
+
+const { commandSpawn } = require("../util/terminal");
 
 const createProjectAction = async (project) => {
   // 1. clone项目
   await download(vueRepo, project, { clone: true });
 
   // 2. 执行 npm install
+  await commandSpawn("npm", ["install"], { cwd: `./${project}` });
 
   // 3. 运行 npm run serve
 
--- 02_learn-cli/lib/util/terminal.js
+++ 02_learn-cli/lib/util/terminal.js
@@ -5,7 +5,14 @@
 const { spawn } = require("child_process");
 
 const commandSpawn = (...args) => {
-  const childProcess = spawn(...args);
+  return new Promise((resolve, reject) => {
+    const childProcess = spawn(...args);
+    childProcess.stdout.pipe(process.stdout);
+    childProcess.stderr.pipe(process.stderr);
+    childProcess.on("close", () => {
+      resolve();
+    });
+  });
 };
 
 module.exports = {
