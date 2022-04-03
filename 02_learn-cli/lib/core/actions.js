3146ed8c6daa5e6ae734832809b421fe6a9058b6
iamzjt-front-end <iamzjt@foxmail.com>
2022-04-03 19:22:40 +0800

    process.platform判断平台

--- 02_learn-cli/lib/core/actions.js
+++ 02_learn-cli/lib/core/actions.js
@@ -6,11 +6,16 @@ const { vueRepo } = require("../config/repo-config");
 const { commandSpawn } = require("../util/terminal");
 
 const createProjectAction = async (project) => {
+  console.log("欢迎使用zjt 脚手架！😊");
+
   // 1. clone项目
+  console.log("正在克隆项目，请稍候...");
   await download(vueRepo, project, { clone: true });
 
   // 2. 执行 npm install
-  await commandSpawn("npm", ["install"], { cwd: `./${project}` });
+  console.log("正在安装依赖，请稍候...");
+  const command = process.platform === "win32" ? "npm.cmd" : "npm";
+  await commandSpawn(command, ["install"], { cwd: `./${project}` });
 
   // 3. 运行 npm run serve
 
