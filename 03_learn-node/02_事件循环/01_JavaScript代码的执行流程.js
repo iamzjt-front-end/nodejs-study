cfea797bcbcff1cb8a4153d031a6d4e185b92471
iamzjt-front-end <iamzjt@foxmail.com>
2022-04-06 21:39:57 +0800

    时间循环

--- 03_learn-node/02_事件循环/01_JavaScript代码的执行流程.js
+++ 03_learn-node/02_事件循环/01_JavaScript代码的执行流程.js
@@ -8,6 +8,7 @@ function sum(num1, num2) {
 
 function foo() {
   const result = sum(20, 30);
+  console.log(result);
 }
 
 foo();
--- 03_learn-node/02_事件循环/02_JavaScript异步setTimeout.js
+++ 03_learn-node/02_事件循环/02_JavaScript异步setTimeout.js
@@ -8,6 +8,11 @@ function sum(num1, num2) {
 
 function foo() {
   const result = sum(20, 30);
+  console.log(result);
 }
+
+setTimeout(() => {
+  console.log("setTimeout");
+}, 1000)
 
 foo();
