a48b6c9d35d0601ebe482e840e5f5230bd9b1509
iamzjt-front-end <iamzjt@foxmail.com>
2022-04-09 23:11:32 +0800

    。。

---
+++ 03_learn-node/04_http模块/02_创建服务器的方式.js
@@ -0,0 +1,26 @@
+const http = require("http");
+
+const serve1 = http.createServer((req, res) => {
+  res.end("Serve1");
+})
+
+serve1.listen(8000, () => {
+  console.log("serve1启动成功~");
+});
+
+// const serve2 = http.createServer((req, res) => {
+//   res.end("Serve2");
+// })
+
+const serve2 = new http.Server((req, res) => {
+  res.end("Serve2");
+});
+
+// serve2.listen(8001, () => {
+//   console.log("serve2启动成功~");
+// });
+
+serve2.listen(() => {
+  console.log("serve2启动成功~");
+  console.log(serve2.address().port);
+});
