3fbab0a406e3ab7f1419f4ccb9b245939f2c456d
iamzjt-front-end <iamzjt@foxmail.com>
2022-04-11 00:20:05 +0800

    文件上传服务器解析

--- 03_learn-node/04_http模块/12_http文件上传-正确的做法.js
+++ 03_learn-node/04_http模块/12_http文件上传-正确的做法.js
@@ -1,23 +1,48 @@
 const http = require("http");
 const url = require("url");
+const qs = require("querystring");
 const fs = require("fs");
 
 const server = http.createServer((req, res) => {
   const { pathname } = url.parse(req.url);
   if (pathname === "/upload") {
     if (req.method === "POST") {
-      const fileWriter = fs.createWriteStream("./foo.png", { flag: "a+" });
-      // req.pipe(fileWriter);
+      req.setEncoding('binary');
+
+      let body = "";
+      const boundary = req.headers["content-type"]
+        .split(";")[1]
+        .replace(" boundary=", "");
+      console.log(boundary);
 
       req.on("data", (data) => {
-        // console.log(data);
-        // fileWriter.write(data);
-      })
+        body += data;
+      });
 
       req.on("end", () => {
-        console.log("文件上传成功~");
-        res.end("文件上传成功~");
-      })
+        console.log(body);
+        // 处理body
+        // 1. 获取image/png的位置
+        const payload = qs.parse(body, "\r\n", ": ");
+        const type = payload["Content-Type"];
+
+        // 2. 开始在image/png的位置进行截取
+        const typeIndex = body.indexOf(type);
+        const typeLength = type.length;
+        let imageData = body.substring(typeIndex + typeLength);
+
+        // 3. 将中间的两个空格去掉
+        imageData = imageData.replace(/^\s\s*/, "");
+
+        // 4. 将最后的boundary去除掉
+        imageData = imageData.substring(0, imageData.indexOf(`\r\n--${boundary}--\r\n`));
+
+        fs.writeFile("./foo.png", imageData, "binary", (err) => {
+          if (!err) {
+            res.end("文件上传成功~");
+          }
+        });
+      });
     }
   }
 });
