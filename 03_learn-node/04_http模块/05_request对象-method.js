cbd732e28da2224dec1194d5c321859cd17854ac
iamzjt-front-end <iamzjt@foxmail.com>
2022-04-10 15:21:54 +0800

    request-method

---
+++ 03_learn-node/04_http模块/05_request对象-method.js
@@ -0,0 +1,27 @@
+const http = require("http");
+const url = require("url");
+const qs = require("querystring");
+
+// 创建一个服务器
+const serve = http.createServer((req, res) => {
+  const { pathname } = url.parse(req.url);
+  if (pathname === "/login") {
+    if (req.method === "POST") {
+      // 拿到body中的数据
+      // console.log(req.body); // 拿不到body
+      req.setEncoding("utf-8");
+      req.on("data", (data) => {
+        // console.log(data, typeof data);
+        // console.log(JSON.parse(data));
+        const { username, password } = JSON.parse(data);
+        console.log(username, password);
+      });
+      res.end("Hello World");
+    }
+  }
+})
+
+// 启动服务器, 并且指定端口号和主机
+serve.listen(8888, '0.0.0.0', () => {
+  console.log("服务器启动成功~");
+});
