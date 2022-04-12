f801505b2d9f35bde5d7447e922cca2f40b49eb1
iamzjt-front-end <iamzjt@foxmail.com>
2022-04-12 19:55:02 +0800

    form-data 解析

--- 03_learn-node/05_express框架/07_中间件-form-data解析.js
+++ 03_learn-node/05_express框架/07_中间件-form-data解析.js
@@ -1,8 +1,16 @@
 const express = require("express");
+const multer = require("multer");
 
 const app = express();
 
+const upload = multer();
+app.use(upload.any());
+
+app.post("/login", (req, res, next) => {
+  console.log(req.body);
+  res.end("登录成功");
+});
 
 app.listen(8888, () => {
   console.log("form-data中间件服务器启动成功~");
-})
+});
