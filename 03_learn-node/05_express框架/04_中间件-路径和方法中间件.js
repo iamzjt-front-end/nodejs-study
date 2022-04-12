ec96351ff01ae4d48a2cc1aa7f690bad4b71371b
iamzjt-front-end <iamzjt@foxmail.com>
2022-04-12 11:24:42 +0800

    路径和方法中间件

---
+++ 03_learn-node/05_express框架/04_中间件-路径和方法中间件.js
@@ -0,0 +1,16 @@
+const express = require("express");
+
+const app = express();
+
+app.use((req, res, next) => {
+  console.log("common middleware 01");
+  next();
+})
+
+app.get("/home", (req, res, next) => {
+  console.log("home path and method middleware 01");
+})
+
+app.listen(8888, () => {
+  console.log("普通中间件服务器启动成功~");
+})
