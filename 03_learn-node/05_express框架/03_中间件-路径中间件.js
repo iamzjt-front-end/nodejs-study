812af0f37b3a4b8b7d234b72d3652349c7dab84b
iamzjt-front-end <iamzjt@foxmail.com>
2022-04-12 10:49:53 +0800

    路径中间件

---
+++ 03_learn-node/05_express框架/03_中间件-路径中间件.js
@@ -0,0 +1,28 @@
+const express = require("express");
+
+const app = express();
+
+app.use((req, res, next) => {
+  console.log("common middleware 01");
+  next();
+})
+
+// 路径匹配中间件
+app.use("/home", (req, res, next) => {
+  console.log("home middleware 01");
+  next();
+});
+
+// 中间插入一个普通中间件
+app.use((req, res, next) => {
+  console.log("common middleware 02");
+  next();
+})
+
+app.use("/home", (req, res, next) => {
+  console.log("home middleware 02");
+});
+
+app.listen(8888, () => {
+  console.log("普通中间件服务器启动成功~");
+})
