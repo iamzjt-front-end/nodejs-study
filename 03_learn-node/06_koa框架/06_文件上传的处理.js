a7b2cd52ccb1b37a2274fa42bd8ac8ef9a07bdd8
iamzjt-front-end <iamzjt@foxmail.com>
2022-04-16 11:40:15 +0800

    ..

--- 03_learn-node/06_koa框架/06_文件上传的处理.js
+++ 03_learn-node/06_koa框架/06_文件上传的处理.js
@@ -1,15 +1,21 @@
 const Koa = require("koa");
+const Router = require("koa-router");
 const multer = require("koa-multer");
 
 const app = new Koa();
-const upload = multer();
-
-app.use(upload.any());
-
-app.use((ctx, next) => {
-  console.log(ctx.req.body);
-  ctx.response.body = "Hello World";
+const uploadRouter = new Router({
+  prefix: "upload",
 });
+const upload = multer({
+  dest: "./uploads/",
+});
+
+uploadRouter.post("/avator", upload.single("avator"), (ctx, next) => {
+  console.log(ctx.req.file);
+  ctx.response.body = "上传成功~";
+});
+
+app.use(uploadRouter.routes());
 
 app.listen(8888, () => {
   console.log("koa文件上传服务器启动成功~");
