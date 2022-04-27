c11a64963326dd08129e25e09d09fab718122292
iamzjt-front-end <iamzjt@foxmail.com>
2022-04-27 14:46:30 +0800

    连接池的使用

---
+++ 04_learn-database/01_mysql2/03_连接池的使用.js
@@ -0,0 +1,20 @@
+const mysql = require("mysql2");
+
+// 1.创建连接池
+const connenctionPool = mysql.createPool({
+  host: "localhost",
+  port: "3306",
+  user: "root",
+  password: "zjtmysql132",
+  database: "coderhub",
+  waitForConnections: "true",
+  connectionLimit: 10,
+  queueLimit: 0,
+});
+
+// 使用连接池
+const statement = `
+  SELECT * FROM products WHERE price > ? AND score > ?;
+`;
+
+connenctionPool .execute(statement, [6000, 7], (err, results) => {});
