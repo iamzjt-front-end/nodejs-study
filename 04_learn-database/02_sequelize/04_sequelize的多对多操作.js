e0f9c1a62b1f05e525026ae578e219f0c5cfb07c
iamzjt-front-end <iamzjt@foxmail.com>
2022-05-01 13:59:23 +0800

    多堆垛

--- 04_learn-database/02_sequelize/04_sequelize的多对多操作.js
+++ 04_learn-database/02_sequelize/04_sequelize的多对多操作.js
@@ -1,10 +1,115 @@
 const { Sequelize, DataTypes, Model, Op } = require("sequelize");
 
 const sequelize = new Sequelize("coderhub", "root", "zjtmysql132", {
-	host: "localhost",
-	dialect: "mysql",
+  host: "localhost",
+  dialect: "mysql",
 });
 
-async function queryProducts() {}
+// students
+class Students extends Model {}
+
+Students.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    age: DataTypes.INTEGER,
+  },
+  {
+    tableName: "students",
+    createdAt: false,
+    updatedAt: false,
+    sequelize,
+  }
+);
+
+// courses
+class Courses extends Model {}
+
+Courses.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price: DataTypes.DOUBLE,
+  },
+  {
+    tableName: "courses",
+    createdAt: false,
+    updatedAt: false,
+    sequelize,
+  }
+);
+
+// studentSelectCourses
+class StudentSelectCourses extends Model {}
+
+StudentSelectCourses.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    studentId: {
+      field: "student_id",
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: Students,
+        key: "id",
+      },
+    },
+    courseId: {
+      field: "course_id",
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: Courses,
+        key: "id",
+      },
+    },
+  },
+  {
+    tableName: "students_select_courses",
+    createdAt: false,
+    updatedAt: false,
+    sequelize,
+  }
+);
+
+// 多对多关系的联系
+Students.belongsToMany(Courses, {
+  through: StudentSelectCourses,
+  foreignKey: "studentId",
+  otherKey: "courseId",
+});
+
+Courses.belongsToMany(Students, {
+  through: StudentSelectCourses,
+  foreignKey: "courseId",
+  otherKey: "studentId",
+});
+
+async function queryProducts() {
+  const result = await Students.findAll({
+    include: {
+      model: Courses,
+    },
+  });
+  console.log(result);
+}
 
 queryProducts();
